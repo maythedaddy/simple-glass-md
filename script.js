a55271d68afe4c516830f88e6da732ffe9febf93
maythedaddy <takuroyatabe@gmail.com>
2023-05-22 21:02:50 +0900

    textarea cutomize done

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const simplemde = new SimpleMDE({
 // Display markdown in HTML preview
 function drawPreview() {
   const html = SimpleMDE.prototype.markdown(simplemde.value());
-  document.getElementById('preview').innerHTML = html;
+  document.querySelector('.preview').innerHTML = html;
 }
 
 document.addEventListener('input', () => drawPreview());
+
+// const easyMDE = new EasyMDE({
+//   element: document.getElementById('my-text-area'),
+// });
+
+// document.addEventListener('input', () => easyMDE.value());
