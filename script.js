d88ddeb67a3393a4babe67f3ebdc2617bc5fbc03
may_the_dadyy <takuroyatabe@gmail.com>
2023-06-11 14:56:21 +0900

    favicon add & フォントサイズ修正 & LOGO add

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const simplemde = new SimpleMDE({
-  element: document.getElementById('editor'),
+  element: document.getElementById("editor"),
   spellChecker: false,
 });
 
 // Display markdown in HTML preview
 function drawPreview() {
   const html = SimpleMDE.prototype.markdown(simplemde.value());
-  document.querySelector('.preview').innerHTML = html;
+  document.querySelector(".preview").innerHTML = html;
 }
 
-document.addEventListener('input', () => drawPreview());
+document.addEventListener("input", () => drawPreview());
 
-// const easyMDE = new EasyMDE({
-//   element: document.getElementById('my-text-area'),
-// });
-
-// document.addEventListener('input', () => easyMDE.value());
+// Update preview when delete key is pressed
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Delete" || event.key === "Backspace") {
+    drawPreview();
+  }
+});
